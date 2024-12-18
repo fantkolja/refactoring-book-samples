e7ef43f94177f3eb70f57c9f90931cbdff31f97d
Mykola Fant <mykola.fant@hazelcast.com>
2024-12-18 12:17:52 +0200

    add simple singleton

---
+++ samples/simple_singleton.cs
@@ -0,0 +1,17 @@
+class Singleton
+{
+  private static Singleton? _instance;
+
+  private Singleton()
+  {
+    Console.WriteLine("Initializing Singleton");
+  }
+
+  public static Singleton GetInstance()
+  {
+    if (Singleton._instance == null) {
+      Singleton._instance = new Singleton();
+    }
+    return Singleton._instance;
+  }
+}
