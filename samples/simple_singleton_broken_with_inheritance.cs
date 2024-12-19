803303ad5740e03cee0bdf9e53e410fbe87a41b3
Mykola Fant <mykola.fant@hazelcast.com>
2024-12-19 11:15:18 +0200

    add simple singleton broken

---
+++ samples/simple_singleton_broken_with_inheritance.cs
@@ -0,0 +1,25 @@
+namespace DesignPatterns.Singleton
+{
+  class Singleton
+  {
+    private static Singleton? _instance;
+
+    private Singleton()
+    {
+      Console.WriteLine("Initializing Singleton");
+    }
+
+    public static Singleton GetInstance()
+    {
+      if (Singleton._instance == null) {
+        Singleton._instance = new Singleton();
+      }
+      return Singleton._instance;
+    }
+
+    public class ChildSingleton : Singleton
+    {
+
+    }
+  }
+}
