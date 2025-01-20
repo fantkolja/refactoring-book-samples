a52441bc343d38cc14fe6712070603b7a8a15d56
Mykola Fant <mykola.fant@hazelcast.com>
2025-01-20 10:28:20 +0200

    add code smell

--- samples/code_smells/primitive_obsession.js
+++ samples/code_smells/primitive_obsession.js
@@ -1,8 +1,41 @@
-// smelly
+// 1.
+// magic numbers
+const productCategory = "laptop";
+
+if (chosenCategory === "laptop") {}
+
+if (userInput === "laptop") {}
+
+if (screenWidth < 600) {}
+
+// constants in global scope
+const LAPTOP_CATEGORY = "laptop"
+// ...
+
+// constants as static properties
+class CategoryHandler {
+	LAPTOP_CATEGORY = "laptop";
+	TABLET_CATEGORY = "tablet";
+	// ...
+}
+
+class Resizer {
+	SMALL_SCREEN_WIDTH = 600;
+	MEDIUM_SCREEN_WIDTH = 600;
+}
+
+
+// enum
+const CATEGORY = {
+	LAPTOP: "laptop",
+	TABLET: "tablet",
+};
+
+// smelly object-like primitive
 const wifePhoneNumber = "+380638976512";
 const friendPhoneNumber = "+380955671274";
 
-// not so smelly
+// not so smelly object
 // const wifePhoneNumber = new PhoneNumber("+38", "0638976512");
 // const friendPhoneNumber = new PhoneNumber("+38", "0955671274");
 
