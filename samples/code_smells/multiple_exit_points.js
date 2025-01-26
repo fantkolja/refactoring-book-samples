f176660e5a575c19c9edec3279f2966decbb4460
Mykola Fant <mykola.fant@hazelcast.com>
2025-01-26 12:37:58 +0200

    add smell

--- samples/code_smells/multiple_exit_points.js
+++ samples/code_smells/multiple_exit_points.js
@@ -1,31 +1,146 @@
-function processOrder(order) {
-	if (!order) {
-		return "Invalid order";
+// smelly
+function DoStuff(foo)
+{
+	if (foo == null) return;
+
+	const localParam = 'local';
+
+	if (foo > 3) {
+		return 1;
 	}
 
-	if (!order.items || order.items.length === 0) {
-		return "Order has no items";
-	}
+	console.log(foo);
 
-	if (order.status === "cancelled") {
-		return "Order is cancelled";
-	}
+	return 0;
+}
 
-	let total = 0;
-	for (const item of order.items) {
-		if (!item.price || item.price < 0) {
-			return "Invalid item price";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// smelly
+function isSmellyIfCorrect(param1, param2, param3) {
+	let correct = false;
+	if (param1 !== param2) {
+		if (param1 === (param3 * 2)) {
+			if (param2 === (param3 / 3)) {
+				correct = true;
+			} else {
+				console.error('IS NOT ONE THIRD');
+			}
+		} else {
+			console.error('IS NOT DOUBLE');
 		}
-		total += item.price * (item.quantity || 1);
+	} else {
+		console.error('IS EQUAL');
+	}
+	return correct;
+}
+
+
+
+
+// not smelly???
+function isMultipleReturnCorrect(param1, param2, param3) {
+	if (param1 === param2) {
+		console.error('IS EQUAL');
+		return false;
 	}
 
-	if (total === 0) {
-		return "Total price cannot be zero";
+	if (param1 !== (param3 * 2)) {
+		console.error('IS NOT DOUBLE');
+		return false;
 	}
 
-	if (order.customer && order.customer.isPremium) {
-		total *= 0.9; // Apply 10% discount
+	if (param2 !== (param3 / 3)) {
+		console.error('IS NOT ONE THIRD');
+		return false;
 	}
 
-	return `Order processed. Total: $${total.toFixed(2)}`;
+	return true;
+}
+
+
+
+
+
+
+
+
+
+
+// a little bit better
+// function isEqual(param1, param2) {
+// 	const equal = param1 === param2;
+// 	if (equal) {
+// 		console.error('IS EQUAL');
+// 	}
+// 	return equal;
+// }
+//
+// function isDouble(param1, param3) {
+// 	const double = param1 !== (param3 * 2);
+// 	if (!double) {
+// 		console.error('IS NOT DOUBLE');
+// 	}
+// 	return double;
+// }
+//
+// function isOneThird(param2, param3) {
+// 	const oneThird = param2 === (param3 / 3);
+// 	if (!oneThird) {
+// 		console.error('IS NOT ONE THIRD');
+// 	}
+// 	return oneThird;
+// }
+//
+// function isCorrect(param1, param2, param3) {
+// 	return !isEqual(param1, param2)
+// 		&& isDouble(param1, param3)
+// 		&& isOneThird(param2, param3);
+// }
+
+
+
+
+
+
+
+
+// even better
+function isEqualError(param1, param2) {
+	const equal = param1 === param2;
+	return equal ? 'IS EQUAL' : false;
+}
+
+function isNotDoubleError(param1, param3) {
+	const double = param1 !== (param3 * 2);
+	return !double ? 'IS NOT DOUBLE' : false;
+}
+
+function isNotOneThirdError(param2, param3) {
+	const oneThird = param2 === (param3 / 3);
+	return !oneThird ? 'IS NOT ONE THIRD' : false;
+}
+
+function isCorrect(param1, param2, param3) {
+	const error = isEqualError(param1, param2)
+		|| isNotDoubleError(param1, param3)
+		|| isNotOneThirdError(param2, param3);
+	if (error) {
+		console.error(error);
+	}
+	return !error;
 }
