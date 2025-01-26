9f51640811b9c768341006b5b18e7ff5c80e0e4c
Mykola Fant <mykola.fant@hazelcast.com>
2025-01-26 12:58:50 +0200

    add smell

--- samples/refactoring_techniques/hide_delegate.js
+++ samples/refactoring_techniques/hide_delegate.js
@@ -13,14 +13,33 @@ class Person {
 	getDepartment() {
 		return this.department
 	}
-
-	getManager() {
-		return this.department.getManager();
-	}
 }
 
 // smelly: client knows too much (inappropriate intimacy) about Person and Department
 function main() {
 	const person = new Person();
-	const manager = person.getManager();
+	const department = person.getDepartment();
+	const manager = department.getManager();
 }
+
+
+
+
+
+
+
+
+// class Person {
+// 	department = new Department();
+// 	getDepartment() {
+// 		return this.department
+// 	}
+//
+// 	getManager() {
+// 		return this.department.getManager();
+// 	}
+// }
+// function main() {
+// 	const person = new Person();
+// 	const manager = person.getManager();
+// }
