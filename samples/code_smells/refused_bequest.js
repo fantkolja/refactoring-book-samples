8aa00b8ea64baf35c85ca4681dbc950d22c192ba
Mykola Fant <mykola.fant@hazelcast.com>
2025-03-27 22:05:19 +0200

    react 18

--- samples/code_smells/refused_bequest.js
+++ samples/code_smells/refused_bequest.js
@@ -23,8 +23,47 @@ class Developer extends Employee {
 		return `${super.getDetails()}, Programming Language: ${this.programmingLanguage}`;
 	}
 
-	// Developer class does not need manageTeam(), causing Refused Bequest
+	// Developer не повинен мати метод manageTeam, але змушений його перевизначати
 	manageTeam() {
 		throw new Error('Developer cannot manage a team he is a slave!');
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// class Employee {
+// 	constructor(name, salary) {
+// 		this.name = name;
+// 		this.salary = salary;
+// 	}
+
+// 	getDetails() {
+// 		return `Name: ${this.name}, Salary: ${this.salary}`;
+// 	}
+// }
+
+// class Manager extends Employee {
+// 	manageTeam() {
+// 		return `${this.name} is managing the team.`;
+// 	}
+// }
+
+// class Developer {
+// 	constructor(employee, programmingLanguage) {
+// 		this.employee = employee;
+// 		this.programmingLanguage = programmingLanguage;
+// 	}
+
+// 	getDetails() {
+// 		return `${this.employee.getDetails()}, Programming Language: ${this.programmingLanguage}`;
+// 	}
+// }
